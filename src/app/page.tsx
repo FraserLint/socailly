8438ddd84d9f9f3bca083103c19f6bcf5dcb9044
Austin Davison <davisonaustin287@gmail.com>
2025-06-09 20:50:23 +0100

    Implemented clerk authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello homepage!</h1>
+      <SignedOut>
+        <SignInButton mode="modal">
+            <button className="bg-red-500">
+                Sign in
+            </button>
+        </SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+          <UserButton />
+      </SignedIn>
     </div>
   );
 }
