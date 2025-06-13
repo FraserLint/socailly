89623789fe2cfb8c752d3650276c99e4f707f6a3
Austin Davison <davisonaustin287@gmail.com>
2025-06-13 23:47:35 +0100

    Updated profile fetching logic for build so compiler is happy

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -2,6 +2,10 @@ import {getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing} from
 import {notFound} from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
 
+type PageProps = {
+    params: { username: string };
+};
+
 export async function generateMetadata({ params }: { params: { username: string } }) {
     const user = await getProfileByUsername(params.username);
 
